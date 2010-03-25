72c2810eee02760d7f47a049002e6beec9d79da0
Duncan Smith <duncanssmith@gmail.com>
2010-03-25 14:29:59 +0000

    Add relationships to installation and version
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,3 +1,4 @@
 class Installation < ActiveRecord::Base
 	belongs_to :device
+	belongs_to :version
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,4 @@
 class Version < ActiveRecord::Base
 	belongs_to :products
-	
+	has_many :installations
 end
