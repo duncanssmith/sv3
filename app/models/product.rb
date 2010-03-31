18f934ab6652ece7e758f6dc993b3a757a6d0f6c
Duncan Smith <duncanssmith@gmail.com>
2010-03-31 13:04:56 +0100

    Complete relation between products and versions Update and New
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,27 @@ class Product < ActiveRecord::Base
 	belongs_to :publisher
 	has_many :versions
 	validates_presence_of :name
+	after_update :save_versions
+	validates_associated :versions
 
 	def version_attributes=(version_attributes)
 	  version_attributes.each do |attributes|	
-      versions.build(attributes)
+			if attributes[:id].blank?
+			  versions.build(attributes)
+			else
+				version = versions.detect { |t| t.id == attributes[:id].to_i }
+				version.attributes = attributes
+			end
+		end
+	end
+
+	def save_versions
+		versions.each do |v|
+			if v.should_destroy?
+				v.destroy
+			else
+				v.save(false)
+			end
 		end
 	end
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -2,4 +2,9 @@ class Version < ActiveRecord::Base
 	belongs_to :product
 	validates_presence_of :name
 	has_many :installations
+	attr_accessor :should_destroy
+
+	def should_destroy?
+		should_destroy.to_i == 1
+	end
 end
