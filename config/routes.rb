ac6afa7601bd2839f4b8af8a3456053c4a2e5003
Duncan Smith <duncanssmith@gmail.com>
2010-04-13 19:39:00 +0100

    Trying to fix bug with login using user_sessions
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
-
   map.resources :licences
 
   map.resources :registers
@@ -16,6 +15,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :assets
   map.resources :publishers
   map.resources :users
+  map.resources :user_sessions
   map.resources :compliances
 
   # The priority is based upon order of creation: first created -> highest priority.
