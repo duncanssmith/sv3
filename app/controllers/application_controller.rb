3d74103dca550fb75047dc6407a09d8e186e69a9
Duncan Smith <duncanssmith@gmail.com>
2010-04-13 12:20:04 +0100

    Clean up the admin test from yesterday
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@
 class ApplicationController < ActionController::Base
 	layout "main"
 	before_filter :authorize, :except => :login
-	before_filter :authorize2, :except => :login
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
@@ -12,16 +11,8 @@ class ApplicationController < ActionController::Base
   # filter_parameter_logging :password
 	filter_parameter_logging "password"
 
-		helper_method :admin?	
-		helper_method :authorize2
 
 		protected
-
-			def authorize2
-				unless admin?
-					flash[:error] = "  *** Unauthorised Access *** "
-				end
-			end
 
 			def authorize
 				#@current_user ||= User.find_by_id(session[:user_id])
@@ -35,7 +26,4 @@ class ApplicationController < ActionController::Base
 
 			end
 
-			def admin? 
-				true
-			end
 end
