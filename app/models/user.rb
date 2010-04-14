461d312abd737b13dd9d5a616675ccbf9e937e9e
Duncan Smith <duncanssmith@gmail.com>
2010-04-14 14:59:52 +0100

    Working on getting Authlogic working
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :client_id
+  attr_accessible :name, :user, :email, :password, :password_confirmation, :client_id
 	acts_as_authentic
 end
