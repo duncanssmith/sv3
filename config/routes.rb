2c3341d72d8f1cded477ec1540b918d6766e2be5
Duncan Smith <duncanssmith@gmail.com>
2010-04-15 11:54:54 +0100

    Added CanCan - Ryan Bates Roles and Abilities code
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
 
+
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
 
+  map.resources :clients
   map.resources :licences
-
   map.resources :registers
   map.resources :installations
   map.resources :entitlements
