f0f8305dfe09a3abf871a7837f3c7754a8cdc473
Duncan Smith <duncanssmith@gmail.com>
2010-04-29 18:18:56 +0100

    Still working on Ajax pagination!!!! AAAARGGHHHH
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,10 +1,17 @@
+ jQuery.ajaxSetup({ 
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+ });
 
-$(document).ready(
-$(function(){
-	$(".pagination a").live("click", function(){
-    $(".pagination").html("Page is loading...");
-		$.get(this.href, null, null, "script");
-		return false;
-	});
-});
-);
+//$(document).ready(
+$(function() {
+	alert("1 hello!");
+  $(".pagination a").live("click", function() {
+    $(".pagination").html( "Page is loading..." );
+    $.get( this.href, null, null, "script" );
+	  alert("2 hello goodbye!");
+    return false;
+  });
+})
+//);
+
+
