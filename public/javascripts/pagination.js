38d99a693b2a4f30e4b2d655d98fd07e10ebf368
Duncan Smith <duncanssmith@gmail.com>
2010-04-29 12:29:53 +0100

    Got jQuery pagination with Ajax to work - finally!
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,11 +1,10 @@
-$(function() {
+
+$(document).ready(
+$(function(){
 	$(".pagination a").live("click", function(){
     $(".pagination").html("Page is loading...");
 		$.get(this.href, null, null, "script");
 		return false;
 	});
 });
-
-jQuery.ajaxSetup({
-	'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-});
+);
