5b120e6cb055b72a775912197590889628336a48
Duncan Smith <duncanssmith@gmail.com>
2010-04-29 15:21:24 +0100

    Add mislav-will_paginate to environment files
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- config/remote_environment.rb
+++ config/remote_environment.rb
@@ -5,8 +5,8 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -30,6 +30,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
 	config.gem "authlogic"
 	config.gem "cancan"
+  config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
