8255e8032c492fe9433bf13cfc9006c1a02ee9c5
Duncan Smith <duncanssmith@gmail.com>
2010-05-07 16:05:08 +0100

    Wrap jQuery calls in a $j to avoid conflict with Prototype JSLibrary
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,15 +1,21 @@
- jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
- });
+var $j = jQuery.noConflict();
+$j(document).ready(function() {
 
+//Enter jQuery code here
 //$(document).ready(
-$(function() {
-  $(".pagination a").live("click", function() {
-    $(".pagination").html( "loading..." );
-    $.get( this.href, null, null, "script" );
+$j(function() {
+  $j(".pagination a").live("click", function() {
+    $j(".pagination").html( "loading page..." );
+    $j.get( this.href, null, null, "script" );
     return false;
   });
 })
 //);
 
+ //$.ajaxSetup({ 
+  //'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+ //});
 
+
+
+});
