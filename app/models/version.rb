d12971aa3dcf5b6ee591697ca53a390d6c12345a
Duncan Smith <duncanssmith@gmail.com>
2010-05-27 12:39:33 +0100

    Add more friendly info to versions templates
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,6 +1,7 @@
 class Version < ActiveRecord::Base
 	belongs_to :product
 	validates_presence_of :name
+	validates_presence_of :product_id
 	has_many :installations
 	attr_accessor :should_destroy
 
