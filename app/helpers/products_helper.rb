3cdc67aac2ea11b37702e362488b6d75b00731be
Duncan Smith <duncanssmith@gmail.com>
2010-06-10 11:32:57 +0100

    Formatting for listings belonging to clients
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,7 @@ module ProductsHelper
 
 	def add_version_link(name)
 		link_to_function name do |page|
-		page.insert_html :bottom, :versions, :partial => 'version', :object => Version.new
+		  page.insert_html :bottom, :versions, :partial => 'version', :object => Version.new
 		end
 	end
 end
