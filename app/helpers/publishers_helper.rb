665ad8ede5a5c9833c0362b871344ca84acbb440
Duncan Smith <duncanssmith@gmail.com>
2010-06-10 11:32:18 +0100

    Remove dead code from Products controller
    Signed-off-by: Duncan Smith <duncanssmith@gmail.com>

--- app/helpers/publishers_helper.rb
+++ app/helpers/publishers_helper.rb
@@ -1,8 +1,2 @@
 module PublishersHelper
-
-	def add_product_link(name)
-		link_to_function name do |page|
-		page.insert_html :bottom, :products, :partial => 'product', :object => Product.new
-	  end
-  end
 end
